a6548c847f637fa7bfa2187fc9c351105d147870
Freet8707 <freet8707@gmail.com>
2020-07-31 12:06:58 -0500

    Updates

---
+++ precache-manifest.98c4ca58aca774e1dcf1ed17fe551134.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5984d17ef7455831ca5b71f820a640ae",
+    "url": "/React-Redux-App/index.html"
+  },
+  {
+    "revision": "d3b2cbb7da85e9268928",
+    "url": "/React-Redux-App/static/css/main.0fe4867f.chunk.css"
+  },
+  {
+    "revision": "27cbc8555af8f226c39c",
+    "url": "/React-Redux-App/static/js/2.d3516473.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Redux-App/static/js/2.d3516473.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3b2cbb7da85e9268928",
+    "url": "/React-Redux-App/static/js/main.cc359439.chunk.js"
+  },
+  {
+    "revision": "ea6ba3dee32752ac4f2f",
+    "url": "/React-Redux-App/static/js/runtime-main.e509ecc4.js"
+  },
+  {
+    "revision": "ae21de0a7ae53e59867d666f246c5650",
+    "url": "/React-Redux-App/static/media/659432.ae21de0a.jpg"
+  }
+]);
